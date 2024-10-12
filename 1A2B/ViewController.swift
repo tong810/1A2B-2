681529a4108f72b7b130f2355c8feb5d57d23267
tong <zz472780@gmail.com>
2024-10-12 15:18:31 +0800

    Initial Commit

---
+++ 1A2B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1A2B
+//
+//  Created by 陳曉潼 on 2024/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 1A2BTests/_A2BTests.swift
@@ -0,0 +1,17 @@
+//
+//  _A2BTests.swift
+//  1A2BTests
+//
+//  Created by 陳曉潼 on 2024/10/12.
+//
+
+import Testing
+@testable import _A2B
+
+struct _A2BTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
